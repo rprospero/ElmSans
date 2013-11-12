68fcaace98f3f25babc73f30c1508440e45ff382
Adam Washington <rprospero@gmail.com>
2013-11-12 02:04:26 -0500

    Improved graph canvas to handle generic points

--- main.elm
+++ main.elm
@@ -25,7 +25,12 @@ getFloat = fromJust 0 . String.toFloat
 rad = labelledField "Radius"
 concentration = labelledField "Concentration"
 
-canvas x y = Graphics.Collage.collage 200 200 [traced (solid lightBlue) <| path [(0,0),(getFloat x,getFloat y)]]
+canvas : [(Float,Float)] -> Element
+canvas points = Graphics.Collage.collage 200 200 [traced (solid lightBlue) <| Graphics.Collage.path points]
+
+makePoints : String -> String -> [(Float,Float)]
+makePoints x y = [(getFloat x, getFloat y),(-1 * getFloat x, getFloat y)]
+
 
 add a b = getFloat a + getFloat b
 
@@ -35,4 +40,9 @@ radcon = lift (plainText . show) <| lift2 add (snd rad) (snd concentration)
 
 scene terms = flow down <| terms
 
-main = lift scene <| combine [lift2 canvas (snd rad) (snd concentration), fst rad, fst concentration, radcon]
+graphCanvas : String -> String -> Element
+graphCanvas x y = canvas <| makePoints x y
+
+
+main = lift scene <| combine [lift2 graphCanvas (snd rad) (snd concentration), fst rad, fst concentration, radcon]
+--main = lift scene <| combine [fst rad, fst concentration, radcon]
