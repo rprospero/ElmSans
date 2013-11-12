2b4a88109ef880321556ee0cadb62242e7294468
Adam Washington <rprospero@gmail.com>
2013-11-12 01:31:35 -0500

    Simplified getFloat function

--- main.elm
+++ main.elm
@@ -1,4 +1,5 @@
 import Graphics.Input
+import Graphics.Collage
 import String
 
 
@@ -20,12 +21,16 @@ fst (a,_) = a
 
 scene terms = flow down terms
 
-                  
+--Helper Function to read a Float
+getFloat = fromJust 0 . String.toFloat
+        
 rad = labelledField "Radius"
 concentration = labelledField "Concentration"
 
-add a b = (fromJust 0 (String.toFloat a)) + (fromJust 0 (String.toFloat b))
+add a b = getFloat a + getFloat b
+
 
 radcon = lift (plainText . show) <| lift2 add (snd rad) (snd concentration)
+
 
 main = lift scene <| combine [fst rad, fst concentration, radcon]
