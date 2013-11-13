ba7103b1228c634ea4660a52dde186221ebcc910
Adam Washington <rprospero@gmail.com>
2013-11-13 17:59:21 -0500

    Added manual adjustment of plot bounds

--- main.elm
+++ main.elm
@@ -18,35 +18,59 @@ fromJust dflt val = maybe dflt id val
 
 fst (a,_) = a
 
-
 --Helper Function to read a Float
 getFloat = fromJust 0 . String.toFloat
-        
+
+liftFloat : Signal String -> Signal Float
+liftFloat = lift getFloat
+
 rad = labelledField "Radius"
 concentration = labelledField "Concentration"
+qmin = labelledField "Q-min"
+qmax = labelledField "Q-max"
+imin = labelledField "I-min"
+imax = labelledField "I-max"
 
-canvas : [(Float,Float)] -> Element
-canvas points = Graphics.Collage.collage 200 200 [traced (solid lightBlue) <| Graphics.Collage.path points]
+
+--Axis stuff
+axisMaker size low high value = (value-low)/(high-low)*size-size/2
+
+xaxis : Signal (Float->Float)
+xaxis = lift2 (axisMaker 200) (liftFloat (snd qmin)) (liftFloat (snd qmax))
+yaxis = lift2 (axisMaker 200) (liftFloat (snd imin)) (liftFloat (snd imax))
+
+projectPoints : (Float->Float) -> (Float->Float) -> [(Float,Float)] -> [(Float,Float)]
+projectPoints fx fy ps = zip (map (fx . fst) ps) (map (fy . snd) ps)
+
+canvas : (Float->Float) -> (Float->Float) -> [(Float,Float)] -> Element
+canvas xax yax points = Graphics.Collage.collage 200 200 [traced (solid lightBlue) <| Graphics.Collage.path (projectPoints xax yax points)]
 
 base : [Float]
-base = [-100..100]
+base = [0..100]
 
 square x y z = (z-getFloat x)*(z-getFloat x)+getFloat y
 
 makePoints : (Float -> Float) -> [(Float,Float)]
 makePoints f = zip base <| map f base
 
+plotPoints : Signal [(Float,Float)]
+plotPoints = lift makePoints (lift2 square (snd rad) (snd concentration))
 
 add a b = getFloat a + getFloat b
-
-
-radcon = lift (plainText . show) <| lift2 add (snd rad) (snd concentration)
 
 scene terms = flow down <| terms
 
 graphCanvas : Signal Element
-graphCanvas = lift canvas <| lift makePoints (lift2 square (snd rad) (snd concentration)) 
+graphCanvas = lift3 canvas xaxis yaxis plotPoints
+
+--testing data
+testread : (Float->Float) -> (Float->Float) -> [(Float,Float)] -> Element
+testread xax yax = plainText . show . head . (projectPoints xax yax)
 
 
-main = lift scene <| combine [graphCanvas , fst rad, fst concentration, radcon]
+main = lift scene <| combine [graphCanvas , fst rad,
+                              fst concentration,
+                              fst qmin, fst qmax,
+                              fst imin, fst imax,
+                              lift3 testread xaxis yaxis plotPoints]
 --main = lift scene <| combine [fst rad, fst concentration, radcon]
