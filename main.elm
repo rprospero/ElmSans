8522fef2f97e2161734222ba58d63ac3e3facb5b
Adam Washington <rprospero@gmail.com>
2013-11-14 02:25:15 -0500

    Setup graph canvas to auto-scale

--- main.elm
+++ main.elm
@@ -3,6 +3,7 @@ module Main where
 import Graphics.Collage
 import FormFactor
 import Util (labelledField)
+import Window
 
 
 qmin = labelledField "Q-min" "0.0"
@@ -10,21 +11,24 @@ qmax = labelledField "Q-max" "100.0"
 imin = labelledField "I-min" "0.0"
 imax = labelledField "I-max" "0.000001"
 
+width=lift (\x -> (x*3) `div` 4) Window.width
+height=lift (\x -> (x*4) `div` 5) Window.height
+
 --Axis stuff
 axisMaker size low high value = (value-low)/(high-low)*size-size/2
 
 xaxis : Signal (Float->Float)
-xaxis = lift2 (axisMaker 200) (snd qmin) (snd qmax)
-yaxis = lift2 (axisMaker 200) (snd imin) (snd imax)
+xaxis = lift3 axisMaker (lift toFloat width) (snd qmin) (snd qmax)
+yaxis = lift3 axisMaker (lift toFloat height) (snd imin) (snd imax)
 
 projectPoints : (Float->Float) -> (Float->Float) -> [(Float,Float)] -> [(Float,Float)]
 projectPoints fx fy ps = zip (map (fx . fst) ps) (map (fy . snd) ps)
 
-canvas : (Float->Float) -> (Float->Float) -> [(Float,Float)] -> Element
-canvas xax yax points = Graphics.Collage.collage 200 200 [traced (solid lightBlue) <| Graphics.Collage.path (projectPoints xax yax points)]
+canvas : Int -> Int -> (Float->Float) -> (Float->Float) -> [(Float,Float)] -> Element
+canvas w h xax yax points = Graphics.Collage.collage w h [traced (solid lightBlue) <| Graphics.Collage.path (projectPoints xax yax points)]
 
 base : [Float]
-base = [0..200]
+base = [0..100]
 
 makePoints : (Float -> Float) -> [(Float,Float)]
 makePoints f = zip base <| map f base
@@ -32,18 +36,20 @@ makePoints f = zip base <| map f base
 plotPoints : Signal [(Float,Float)]
 plotPoints = lift makePoints (lift FormFactor.hardSphere FormFactor.hardParams)
 
-scene terms = flow down <| terms
+scene terms = flow right <| terms
 
 graphCanvas : Signal Element
-graphCanvas = lift3 canvas xaxis yaxis plotPoints
+graphCanvas = lift5 canvas width height xaxis yaxis plotPoints
 
 --testing data
 testread : (Float->Float) -> (Float->Float) -> [(Float,Float)] -> Element
 testread xax yax = plainText . show . head . (projectPoints xax yax)
 
 
-main = lift scene <| combine [graphCanvas , FormFactor.hardBox,
-                              fst qmin, fst qmax,
-                              fst imin, fst imax,
-                              lift3 testread xaxis yaxis plotPoints]
+sizeBox = foldl (lift2 above) (fst imax) (map fst [imin, qmax, qmin])
+
+main = lift scene <| combine [graphCanvas , lift2 above FormFactor.hardBox 
+                              sizeBox,
+                              lift3 testread xaxis yaxis plotPoints,
+                              lift (plainText . show) height]
 
