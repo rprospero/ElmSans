cf1279b2c7d4c21c454d27870221eec7b3f93adc
Adam Washington <rprospero@gmail.com>
2013-11-14 19:57:02 -0500

    Changed slider to use signalFlipper

--- collapse.elm
+++ collapse.elm
@@ -15,23 +15,21 @@ dropper op h = height h (segment op)
 
 dbox op h = flow down [dropper op h, plainText "Next"]
 
-move2 : (Int,Int) -> (Bool,Time.Time) -> (Bool,(Bool,Int)) -> ((Bool,(Bool,Int)),(Bool,Int))
-move2 (bottom,top) (click,_) (oldclick,(stt,value)) =
-  let goalState = if (click && (not oldclick)) then not stt else stt
-      goal = if goalState then top else bottom
+move2 : (Int,Int) -> (Bool,Time.Time) -> Int -> Int
+move2 (bottom,top) (click,_) value =
+  let goal = if click then top else bottom
       newVal = if value > goal then value - 1
                else if value < goal
                     then value + 1
                     else value
-      output = (goalState,newVal)
-  in ((click,output),output)
+  in newVal
 
 tuple a b = (a,b)
 
-clickTimer = lift2 tuple (Signal.dropRepeats Mouse.isDown) (Time.every (15 * millisecond))
+clickTimer = lift2 tuple (signalFlipper Mouse.isDown) (Time.every (15 * millisecond))
 
-slider : Automaton.Automaton (Bool,Time.Time) (Bool,Int)
-slider = Automaton.hiddenState (False,(False,0)) (move2 (100,10))
+slider : Automaton.Automaton (Bool,Time.Time) Int
+slider = Automaton.state 0 (move2 (100,10))
 
 flipper : Bool -> (Bool,Bool) -> ((Bool,Bool),Bool)
 flipper click (oldclick,state) =
@@ -42,6 +40,6 @@ signalFlipper : Signal Bool -> Signal Bool
 signalFlipper = Automaton.run (Automaton.hiddenState (False,False) flipper) False     
 
 main = lift (flow right) <| combine [lift2 dbox (lift (\x -> if x then 0.01 else 0.99) Mouse.isDown) <|
-                                     lift snd (Automaton.run slider (False,100) clickTimer),
+                                     (Automaton.run slider 100 clickTimer),
                                      lift (plainText . show) clickTimer,
                                      lift (plainText . show) <| signalFlipper Mouse.isDown]
