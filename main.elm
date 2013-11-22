c1e1bf4bb388542f76ca2876ad5cc9471202ad6c
Adam Washington <rprospero@gmail.com>
2013-11-22 17:36:52 -0500

    Finished pulling graph work out of main

--- main.elm
+++ main.elm
@@ -1,12 +1,13 @@
 module Main where
 
-import Graphics.Collage
+--import Graphics.Collage
 import Graphics.Input
 import FormFactor
 import Util (labelledField,labelledChoice,range,labove,getFloat)
 import Window
 import Collapse
 import Fields (updater,fieldMaker)
+import Graph (makePoints,Axis,Linear,Log,axisMaker,canvas)
 
 
 qGroup = Graphics.Input.fields ("Name","Value")
@@ -27,46 +28,23 @@ qFields = flow down <| [fieldMaker qGroup "qmin" "0.001",
 (qButton,qCollapse) = Collapse.collapsibleSignal qTitle qFields
 qBox = Collapse.collapsible qButton qFields
 
---qmin = labelledField "Q-min" "0.0"
---qmax = labelledField "Q-max" "100.0"
---qcount = labelledField "Q-samples" "100"
 
-width=lift (\x -> (x*3) `div` 4) Window.width
-height=lift (\x -> (x*4) `div` 5) Window.height
-
---Axis stuff
-
-data Axis = Linear | Log
-
-log = logBase 10
-
-axisMaker kind size low high value = 
-          case kind of
-               Linear -> (value-low)/(high-low)*size-size/2
-               Log ->  let v = log value
-                           l = log low
-                           h = log high
-                       in (v-l)/(h-l)*size-size/2
-
---yaxisKind = Graphics.Input.dropDown [("Linear",Linear),("Log",Log)]
 xaxisKind = labelledChoice "X-axis" [("Linear",Linear),("Log",Log)]
 yaxisKind = labelledChoice "Y-axis" [("Linear",Linear),("Log",Log)]
+
 
 xaxis : Signal (Float->Float)
 xaxis = lift4 axisMaker (snd xaxisKind) (lift toFloat width) (lift (\x -> x.qmin) qSignal) (lift (\x -> x.qmax) qSignal)
 yaxis = lift4 axisMaker (snd yaxisKind) (lift toFloat height) (lift (foldr1 min . map snd)  plotPoints) (lift (foldr1 max . map snd) plotPoints)
 
-projectPoints : (Float->Float) -> (Float->Float) -> [(Float,Float)] -> [(Float,Float)]
-projectPoints fx fy ps = zip (map (fx . fst) ps) (map (fy . snd) ps)
+--qmin = labelledField "Q-min" "0.0"
+--qmax = labelledField "Q-max" "100.0"
+--qcount = labelledField "Q-samples" "100"
 
-canvas : Int -> Int -> (Float->Float) -> (Float->Float) -> [(Float,Float)] -> Element
-canvas w h xax yax points = Graphics.Collage.collage w h [traced (solid lightBlue) <| Graphics.Collage.path (projectPoints xax yax points)]
 
---base : [Float]
---base = range 0 100 1
+--Axis stuff
 
-makePoints : [Float] -> (Float -> Float) -> [(Float,Float)]
-makePoints base f = zip base <| map f base
+
 
 plotPoints : Signal [(Float,Float)]
 plotPoints = lift2 makePoints (lift qRange qSignal) (lift FormFactor.hardSphere FormFactor.hsSignal)
@@ -77,11 +55,15 @@ graphCanvas : Signal Element
 graphCanvas = lift5 canvas width height xaxis yaxis plotPoints
 
 --testing data
-testread : (Float->Float) -> (Float->Float) -> [(Float,Float)] -> Element
-testread xax yax = plainText . show . head . (projectPoints xax yax)
+--testread : (Float->Float) -> (Float->Float) -> [(Float,Float)] -> Element
+--testread xax yax = plainText . show . head . (projectPoints xax yax)
 
 
 --sizeBox = foldl (lift2 above) (fst qcount) (map fst [qmax, qmin])
+
+width=lift (\x -> (x*3) `div` 4) Window.width
+height=lift (\x -> x - 20) Window.height
+
 
 main = lift scene <| combine [graphCanvas , lift FormFactor.hsBox FormFactor.hsCollapse `labove`
                                             lift qBox qCollapse `labove`
