67619bcdfe5fd396daf97e3ca401ad76fa79da89
Adam Washington <rprospero@gmail.com>
2013-11-22 21:02:01 -0500

    Started adding Grid lines

--- Graph.elm
+++ Graph.elm
@@ -21,10 +21,16 @@ projectPoints : (Float->Float) -> (Float->Float) -> [(Float,Float)] -> [(Float,F
 projectPoints fx fy ps = zip (map (fx . fst) ps) (map (fy . snd) ps)
 
 canvas : Int -> Int -> (Float->Float) -> (Float->Float) -> [(Float,Float)] -> Element
-canvas w h xax yax points = Graphics.Collage.collage w h [traced (solid lightBlue) <| Graphics.Collage.path (projectPoints xax yax points)]
+canvas w h xax yax points = Graphics.Collage.collage w h [traced (solid lightBlue) <| Graphics.Collage.path (projectPoints xax yax points), xGrid h xax, yGrid h yax]
 
 --base : [Float]
 --base = range 0 100 1
+
+xGrid h xax = group <| map (traced (solid darkGrey) . Graphics.Collage.path . projectPoints xax id . \x -> [(x,(toFloat h)/(-2)),(x,(toFloat h)/2)]) [1,2,3,4,5,6,7,8,9,10]
+
+
+yGrid h yax = group <| map (traced (solid darkGrey) . Graphics.Collage.path . projectPoints id yax . \y -> [((toFloat h)/(-2),y),((toFloat h)/2,y)]) [1,2,3,4,5,6,7,8,9,10]
+
 
 makePoints : [Float] -> (Float -> Float) -> [(Float,Float)]
 makePoints base f = zip base <| map f base
