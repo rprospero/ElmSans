887b1121fb88d687dde8410ab007f3f44b92d48c
Adam Washington <rprospero@gmail.com>
2013-12-01 18:22:40 -0500

    Removed dead code from Graph.

--- Graph.elm
+++ Graph.elm
@@ -21,17 +21,11 @@ ticMaker kind =
     Linear -> smartTics 10
     Log -> map (\x -> e^x) . smartTics 10 . map log
 
---yaxisKind = Graphics.Input.dropDown [("Linear",Linear),("Log",Log)]
-
-
 projectPoints : (Float->Float) -> (Float->Float) -> [(Float,Float)] -> [(Float,Float)]
 projectPoints fx fy ps = zip (map (fx . fst) ps) (map (fy . snd) ps)
 
 canvas : Int -> Int -> (Float->Float) -> Axis -> (Float->Float) -> Axis -> [(Float,Float)] -> Element
 canvas w h xax xKind yax yKind points = Graphics.Collage.collage w h [traced (solid lightBlue) <| Graphics.Collage.path (projectPoints xax yax points), xGrid h xax . ticMaker xKind <| map fst points, yGrid w yax . ticMaker yKind <| map snd points]
-
---base : [Float]
---base = range 0 100 1
 
 smartTics count pts = let low = minimum pts
                           high = maximum pts
