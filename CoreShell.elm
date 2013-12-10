5201e0a36764e7fca88b6a771aaf80a8b7eecdde
Adam Washington <rprospero@gmail.com>
2013-12-10 17:55:24 -0500

    Confirmed CoreShell results

--- CoreShell.elm
+++ CoreShell.elm
@@ -17,13 +17,13 @@ coreShell params q =
   let rc = params.coreRadius
       t = params.shellThickness
       rs = rc + t
-      vc = 4.0*pi/3.0*rc^3
-      vs = 4.0*pi/3.0*rs^3
-      j = \x -> (sin x - (x * cos x))/(x^3)
+      vc = 4.0*pi/3.0*(rc^3)
+      vs = 4.0*pi/3.0*(rs^3)
+      j = \x -> ((sin x) - (x * cos x))/(x^3)
   in
-   params.scale/vc * (3 * vc * (params.coreSLD - params.shellSLD)*j(q*rc)/(q*rc)
-                     +
-                     3 * vs * (params.shellSLD - params.solventSLD)*j(q*rs)/(q*rs)) + params.bgd
+   10^8*params.scale/vs * (3 * vc * (params.coreSLD - params.shellSLD)*j(q*rc)
+                           +
+                           3 * vs * (params.shellSLD - params.solventSLD)*j(q*rs))^2 + params.bgd
 
 csCondenser (name,val) rec = case name of
                              "Scale" -> {rec - scale | scale = getFloat val}
@@ -40,9 +40,9 @@ csgroup = fields ("Name","1.0")
 csSignal = updater csCondenser {scale=1.0,
                                 coreRadius=60.0,
                                 shellThickness=10.0,
-                                coreSLD = 10^(-6),
-                                shellSLD = 2*10^(-6),
-                                solventSLD = 3*10^(-6),
+                                coreSLD = 0.000001,
+                                shellSLD = 0.000002,
+                                solventSLD = 0.000003,
                                 bgd=0.0} csgroup.events
 
 
@@ -52,7 +52,7 @@ csLabelWidth = widthOf . plainText . show <| "ShellThickness"
 csFields = flow down <| [fieldMaker csgroup csLabelWidth "Scale" "1.0",
                          fieldMaker csgroup csLabelWidth "Core Radius" "60.0",
                          fieldMaker csgroup csLabelWidth "Shell Thickness" "10.0",
-                         fieldMaker csgroup csLabelWidth "Core SLD" "1.0e-6",
-                         fieldMaker csgroup csLabelWidth "Shell SLD" "2.0e-6",
-                         fieldMaker csgroup csLabelWidth "SolventSLD" "3.0e-6",
+                         fieldMaker csgroup csLabelWidth "Core SLD" "0.000001",
+                         fieldMaker csgroup csLabelWidth "Shell SLD" "0.000002",
+                         fieldMaker csgroup csLabelWidth "SolventSLD" "0.000003",
                          fieldMaker csgroup csLabelWidth "Background" "0.0"]
